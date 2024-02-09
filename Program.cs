8852b237a7ea821269a15b2355ae47c2d7e1c686
Jenyffer Prochno <jlprochno@gmail.com>
2024-02-09 10:04:36 -0300

    Publishing

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace temperatureFahrenheit;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Olá, por gentileza, digite quantos graus você deseja converter: ");
+        double degrees = Convert.ToDouble(Console.ReadLine());
+
+        double fahrenheit = (degrees * 9/5) + 32;
+
+        Console.WriteLine($"{degrees}°C são {fahrenheit}ºF.");
+    }
+}
